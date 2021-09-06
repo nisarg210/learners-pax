9c5a1bb8aa8ba062e57b5db06cbd1f225e990797
Nisarg <nisarg.prajapati21101@gmail.com>
2021-09-06 14:41:57 +0530

    upload err fix & document model added

---
+++ server/models/document.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const documentSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  teacher: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  branch: {
+    type: String,
+    required: true,
+  },
+  semester: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("document", documentSchema);
--- server/routes/api/notes.js
+++ server/routes/api/notes.js
@@ -1,25 +1,28 @@
 const express = require("express");
 // const Announcement = require("../../models/announcements");
 const router = express.Router();
-const multer =require("multer");
-const upload = multer({ dest: 'uploads/' })
-
-const google_upload = require("../../gapi/drive")
-router.post('/upload', upload.single('file'), async function (req, res) {
-    try {
-        // branch:req.body.branch,
-        // semester:req.body.semester,
-        const file={
-            name:req.body.name,
-            filepath:req.file.path,
-            mimeType:req.file.mimetype
-        }
-        const response = await google_upload(file);
-        
-        res.send(response);
-    } catch (error) {
-        res.send(400)
-    }
-    
-  })
-  module.exports = router;
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const fs = require("fs");
+const google_upload = require("../../gapi/drive");
+router.post("/upload", upload.single("file"), async function (req, res) {
+  try {
+    // branch:req.body.branch,
+    // semester:req.body.semester,
+    const file = {
+      name: req.body.name,
+      filepath: req.file.path,
+      mimeType: req.file.mimetype,
+    };
+    const response = await google_upload(file);
+    console.log("req.file.path");
+    fs.unlink(req.file.path, (error) => {
+      console.log(error);
+    });
+    res.json({ id: response });
+  } catch (error) {
+    // fs.unlinkSync(req.file.path);
+    res.send(error);
+  }
+});
+module.exports = router;
